e15ef200ccd9771e6d77ec1958d64d2db243b473
Jimmy <rkdfnql22@gmail.com>
2024-08-27 18:08:39 +0900

    🎨 Add: home 추가

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './home.module.scss'
+
+const page = () => {
+  return (
+    <div className={style.container}>
+      home
+    </div>
+  );
+};
+
+export default page;
