013eaf26279a0c211625d7e2d43b366d6fc2388c
Jimmy <rkdfnql22@gmail.com>
2024-08-28 14:02:21 +0900

    🎨 Add: wagmi config 추가

---
+++ lib/config.ts
@@ -0,0 +1,20 @@
+'use client'
+import { http, createConfig } from 'wagmi'
+import { base, mainnet, optimism } from 'wagmi/chains'
+import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
+
+const projectId = process.env.NEXT_PUBLIC_WAGMI_PROJECT_ID as string // https://cloud.walletconnect.com/ id 발급
+
+export const config = createConfig({
+  chains: [mainnet, base],
+  connectors: [
+    injected(),
+    walletConnect({ projectId }),
+    metaMask(),
+    safe(),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+  },
+})
