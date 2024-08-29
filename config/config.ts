b4b4d589a72d2ad03844bc012d74b1889a8f7a93
Jimmy <rkdfnql22@gmail.com>
2024-08-29 13:51:55 +0900

    ♻️ Refactor: rainbowkit 사용하여 변경

--- config/config.ts
+++ config/config.ts
@@ -1,20 +1,23 @@
 'use client'
-import { http, createConfig } from 'wagmi'
-import { base, mainnet, optimism } from 'wagmi/chains'
-import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
+import { http } from 'wagmi';
+import {
+  mainnet,
+  bsc,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains'
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 
 const projectId = process.env.NEXT_PUBLIC_WAGMI_PROJECT_ID as string // https://cloud.walletconnect.com/ id 발급
 
-export const config = createConfig({
-  chains: [mainnet, base],
-  connectors: [
-    injected(),
-    walletConnect({ projectId }),
-    metaMask(),
-    safe(),
-  ],
+export const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: projectId,
+  chains: [mainnet, bsc, polygon, optimism, arbitrum, base],
+  ssr: true, // 디앱이 서버 측 렌더링(SSR)을 사용하는 경우
   transports: {
-    [mainnet.id]: http(),
-    [base.id]: http(),
-  },
+    [mainnet.id]: http()
+  }
 })
